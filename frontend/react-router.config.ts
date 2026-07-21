c81ad359362b6a4676f6791aeb9ccf967bf867c9
Joshua <joshuaezemihediwa@gmail.com>
2026-07-21 14:30:28 +0100

    Final code

--- frontend/react-router.config.ts
+++ frontend/react-router.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
+
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
+  presets: [vercelPreset()],
   ssr: true,
   future: {
     v8_middleware: true,
@@ -10,5 +13,12 @@ export default {
     v8_splitRouteModules: true,
     v8_trailingSlashAwareDataRequests: true,
     v8_viteEnvironmentApi: true,
+    
   },
+
+
+
+   
+
+
 } satisfies Config;
